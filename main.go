07ff84e734d6b15c309f67257ebbf0577445aa89
Wechuli <wechulipaul@yahoo.com>
2020-02-24 09:53:45 +0300

    main boilerplate

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+)
+
+const (
+	dbPort = 5432
+)
+
+func main() {
+
+	host, user, password, dbname := os.Getenv("RSS_DB_HOST"), os.Getenv("RSS_DB_USERNAME"), os.Getenv("RSS_DB_PASSWORD"), os.Getenv("RSS_DB_NAME")
+
+	fmt.Println("Starting program")
+	app := App{}
+	app.Initialize(host, dbPort, user, password, dbname)
+	app.Run(":8080")
+
+}
